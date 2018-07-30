813cf47c35342590b85902c17bec8e0f6d7cdcd3
MJ2P <mj2p.sg@gmail.com>
2018-07-30 21:15:52 +0100

    first commit

--- __init__.py
+++ __init__.py
@@ -1,14 +1,25 @@
-from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill
+from .alexa_pattern import AlexaLedPattern
 
 
 class RespeakerFourHat(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.alexa_pattern = AlexaLedPattern()
 
-    @intent_file_handler('hat.four.respeaker.intent')
-    def handle_hat_four_respeaker(self, message):
-        self.speak_dialog('hat.four.respeaker')
+    def initialize(self):
+        self.add_event('recognizer_loop:wakeword', self.handle_wakeword)
+        self.add_event('recognizer_loop:record_begin', self.handle_record_begin)
+        self.add_event('speak', self.handle_speak)
+
+    def handle_wakeword(self, message):
+        self.alexa_pattern.wakeup()
+
+    def handle_record_begin(self, message):
+        self.alexa_pattern.listen()
+
+    def handle_speak(self, message):
+        self.alexa_pattern.speak()
 
 
 def create_skill():
